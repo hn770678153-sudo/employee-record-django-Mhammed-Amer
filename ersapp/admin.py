40619ae26a49cfccf16babd1f516c4f657835724
Mhammed Amer <mhammed@example.com>
2026-01-21 23:32:58 +0300

    Initial commit  upload project

---
+++ ersapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Employees)
+admin.site.register(empeducation)
+admin.site.register(empexperience)
+
+# Username: mohammed
+# Password: 11220
+
+
